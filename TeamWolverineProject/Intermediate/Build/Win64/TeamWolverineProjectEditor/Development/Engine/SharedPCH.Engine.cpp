7f4d1971694f84b6d4a492e77c9c89319ba6e5e3
mjaupau <53095138+mjaupau@users.noreply.github.com>
2019-07-19 20:14:02 +0200

    Unreal base project
    Just an empty Unreal project.

---
+++ TeamWolverineProject/Intermediate/Build/Win64/TeamWolverineProjectEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/TeamWolverine/TeamWolverineProject/Intermediate/Build/Win64/TeamWolverineProjectEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ TeamWolverineProject/Source/TeamWolverineProject/TeamWolverineProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TeamWolverineProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMWOLVERINEPROJECT_API ATeamWolverineProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
